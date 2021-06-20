794fc2be77400aa6ac35aec36cbc22dc41fcf02a
revolveheld <revolveheld@gmail.com>
2021-06-21 02:23:58 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:api_app/cart_c.dart';
 import 'package:api_app/Repository.dart';
 import 'package:api_app/dummy.dart';
@@ -35,7 +34,6 @@ class HttpScreen extends StatefulWidget {
 
   @override
   _HttpScreen createState() => _HttpScreen();
-
 }
 
 class _HttpScreen extends State<HttpScreen> {
@@ -51,50 +49,47 @@ class _HttpScreen extends State<HttpScreen> {
         // print("dadaw");
         // print(valueStreamDb.length);
         var arrayCart = [];
-        for(var valueCart in valueStreamDb){
+        for (var valueCart in valueStreamDb) {
           arrayCart.add(valueCart.id);
         }
         cartCount = arrayCart.toSet().toList().length;
-        setState(() {
-        });
+        setState(() {});
       });
     });
   }
-  void getCart4Grid() async{
+
+  void getCart4Grid() async {
     // var ListCart = await Repository().getCart();
     //   print(ListCart);
 
-    Repository().getCart().then((response){
+    Repository().getCart().then(
+      (response) {
         isLoading = 2;
         carts = response;
-        setState(() {
-        });
-    },
-        onError: (exception){
-      if(exception.message != null ){
-        isLoading = 3;
-        setState(() {
-        });
-      }
-    },);
+        setState(() {});
+      },
+      onError: (exception) {
+        if (exception.message != null) {
+          isLoading = 3;
+          setState(() {});
+        }
+      },
+    );
   }
-  _body (context){
+
+  _body(context) {
     print(isLoading);
-    if(isLoading == 1){
+    if (isLoading == 1) {
       return Center(
-          child: CircularProgressIndicator(),
+        child: CircularProgressIndicator(),
       );
-    }
-    else if(isLoading == 3){
+    } else if (isLoading == 3) {
       return Center(
         child: Text("gagal mendapatkan barang dari server"),
       );
-    }
-    else {
-      return
-      Container(
-        child :
-          GridView.builder(
+    } else {
+      return Container(
+          child: GridView.builder(
               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 2,
                 childAspectRatio: MediaQuery.of(context).size.width /
@@ -103,110 +98,130 @@ class _HttpScreen extends State<HttpScreen> {
               itemCount: carts?.length ?? 0,
               itemBuilder: (BuildContext context, index) {
                 return GestureDetector(
-                    child:  Card(
-                      color: Colors.white,
-
-                      child: Container(
-                        padding: EdgeInsets.all(8),
-                        child: Column(
-                          children: [
-                            Image.network(
-                                "https://ranting.twisdev.com/uploads/${carts[index].foto}"),
-                            SizedBox(height: 10,),
-                            Row(
-                              children: [
-                            Container (
-                                  padding: const EdgeInsets.all(0),
-                                  width: MediaQuery.of(context).size.width/2.5,
-                                  child: new Row (
-                                    children:[
-                                      Flexible(child: Text(carts[index].title),),
-                                    ],
+                    child: Card(
+                        color: Colors.white,
+                        child: Container(
+                          padding: EdgeInsets.all(8),
+                          child: Column(
+                            children: [
+                              Flexible(
+                                  child: Image.network(
+                                      "https://ranting.twisdev.com/uploads/${carts[index].foto}")),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Row(
+                                children: [
+                                  Container(
+                                    padding: const EdgeInsets.all(0),
+                                    width:
+                                        MediaQuery.of(context).size.width / 2.5,
+                                    child: new Row(
+                                      children: [
+                                        Flexible(
+                                          child: Text(carts[index].title),
+                                        ),
+                                      ],
+                                    ),
                                   ),
-                                ),
-                              ],
-                            ),
-                            SizedBox(height: 7,),
-                            Row(
-                              children: [
-                                Column(
-                                  children: [
-                                    Text("Rp. ${carts[index].price}",),
-                                  ],
-                                )
-                              ],
-                            ),
-                            SizedBox(height: 7,),
-                            Row(
-                              children: [
-                                Icon(Icons.maps_ugc,size: 20,),
-                                Text(carts[index].location_name),
-                              ],
-                            ),
-                            SizedBox(height: 7,),
-                            Container(
-                              // color: Colors.red,
-                              child:
+                                ],
+                              ),
+                              SizedBox(
+                                height: 7,
+                              ),
                               Row(
                                 children: [
                                   Column(
                                     children: [
-                                      Row(
-                                        children: [
-                                          Icon(Icons.account_circle,size: 20,),
-                                          Text(carts[index].username),
-                                        ],
+                                      Text(
+                                        "Rp. ${carts[index].price}",
                                       ),
-                                      // if(carts[index] != "")
-                                      if(int.parse(carts[index].is_sold_out ?? 0) > 0)
+                                    ],
+                                  )
+                                ],
+                              ),
+                              SizedBox(
+                                height: 7,
+                              ),
+                              Row(
+                                children: [
+                                  Icon(
+                                    Icons.maps_ugc,
+                                    size: 20,
+                                  ),
+                                  Text(carts[index].location_name),
+                                ],
+                              ),
+                              SizedBox(
+                                height: 7,
+                              ),
+                              Container(
+                                // color: Colors.red,
+                                child: Row(
+                                  children: [
+                                    Column(
+                                      children: [
                                         Row(
                                           children: [
-                                            SizedBox(height: 50,),
-                                            Container(
-                                              color: Colors.blueAccent,
-                                              padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                                              child: Text("Ready Stock", style: TextStyle(color: Colors.white),),
-                                            )
+                                            Icon(
+                                              Icons.account_circle,
+                                              size: 20,
+                                            ),
+                                            Text(carts[index].username),
                                           ],
                                         ),
-                                    ],
-                                  ),
-                                  Container(
-                                    alignment: Alignment.topRight,
-                                    child:
-                                      Column(
-                                      children: [
-                                        if(int.parse(carts[index].halal) > 0)
+                                        // if(carts[index] != "")
+                                        if (int.parse(
+                                                carts[index].is_sold_out ?? 0) >
+                                            0)
                                           Row(
                                             children: [
-                                              // Spacer(),
-                                              Image.network(
-                                                "https://seeklogo.com/images/H/halal-logo-150ED752BD-seeklogo.com.png", height: 35,),
+                                              SizedBox(
+                                                height: 50,
+                                              ),
+                                              Container(
+                                                color: Colors.blueAccent,
+                                                padding: EdgeInsets.fromLTRB(
+                                                    10, 10, 10, 10),
+                                                child: Text(
+                                                  "Ready Stock",
+                                                  style: TextStyle(
+                                                      color: Colors.white),
+                                                ),
+                                              )
                                             ],
                                           ),
                                       ],
                                     ),
-                                  )
-
-                                ],
-                              ),
-                            )
-
-
-                          ],
-                        ),
-                      )
-                    ),
+                                    Container(
+                                      alignment: Alignment.topRight,
+                                      child: Column(
+                                        children: [
+                                          if (int.parse(carts[index].halal) > 0)
+                                            Row(
+                                              children: [
+                                                // Spacer(),
+                                                Image.network(
+                                                  "https://seeklogo.com/images/H/halal-logo-150ED752BD-seeklogo.com.png",
+                                                  height: 35,
+                                                ),
+                                              ],
+                                            ),
+                                        ],
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        )),
                     onTap: () {
                       setState(() {
                         Repository().insertCart(carts[index]);
                       });
-                    }
-                );
-              }
-          )
-      );
-
+                    });
+              }));
     }
   }
 
@@ -217,49 +232,44 @@ class _HttpScreen extends State<HttpScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Test Flutter"),
-         actions: [
-           Stack(
-             children:[
-               IconButton(
-                 icon: SizedBox(
-                     height: 30,
-                     width: 22,
-                     child: Icon(Icons.shopping_bag)
-                 ),
-                 onPressed: () {
-                   Navigator.push(
-                     context,
-                     MaterialPageRoute(builder: (context) => showCart()),
-                   );
-                 },
-               ),
-               if(cartCount > 0)
-               Positioned(
-                 right: 12,
-                 top: 3,
-                 child: Container(
-                   color: Colors.red,
-                   padding: EdgeInsets.all(1),
-                   child:
-                   Text('$cartCount', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 13)),
-                 )
-               ),
-            ]
-           )
-
-         ],
+        actions: [
+          Stack(children: [
+            IconButton(
+              icon: SizedBox(
+                  height: 30, width: 22, child: Icon(Icons.shopping_bag)),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => showCart()),
+                );
+              },
+            ),
+            if (cartCount > 0)
+              Positioned(
+                  right: 12,
+                  top: 3,
+                  child: Container(
+                    color: Colors.red,
+                    padding: EdgeInsets.all(1),
+                    child: Text('$cartCount',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 13)),
+                  )),
+          ])
+        ],
       ),
       body: _body(context),
     );
   }
+
   void showDialog1(BuildContext context) {
     showDialog(
         context: context,
         builder: (context) => AlertDialog(
-          title: Text("Alert"),
-          content: Text("Alert aasasasasas"),
-        )
-    );
+              title: Text("Alert"),
+              content: Text("Alert aasasasasas"),
+            ));
   }
 }
-
